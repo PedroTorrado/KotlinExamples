302b6b1fecfc5daa08eaf57c4e48a726c13523a8
PedroTorrado <88887442+PedroTorrado@users.noreply.github.com>
2021-12-02 18:35:06 +0000

    Update Classes and inheritance in Kotlin.kt

--- Classes and inheritance in Kotlin.kt
+++ Classes and inheritance in Kotlin.kt
@@ -6,15 +6,16 @@ fun main() {
     val roundHut = RoundHut(3, 10.0)
     val roundTower = RoundTower(4, 15.5)
 
+    // by using the "with()" function it makes it easier to read and less error prone 
     with(squareCabin){
-    	println("\nSquare Cabin \n===========")
-    	println("Capacity: ${capacity}")
-    	println("Material: ${buildingMaterial}")
-    	println("Has Room? ${hasRoom()}")
-        println("Floor Area: %.2f".format(floorArea()))
-        println("Has room? ${hasRoom()}")
-        getRoom()
-        println("Has room? ${hasRoom()}")
+    	println("\nSquare Cabin \n===========") 
+    	println("Capacity: ${capacity}") // same as println("Capacity: ${squareCabin.capacity}"
+    	println("Material: ${buildingMaterial}") //same as println("Material: ${squareCabin.buildingMaterial}")
+    	println("Has Room? ${hasRoom()}") // same as println("Has Room? ${squareCabin.hasRoom()}")
+        println("Floor Area: %.2f".format(floorArea())) // %.2f".format(floorArea()), formats the string to 2 decimal characters
+        println("Has room? ${hasRoom()}") // same as println("Has Room? ${squareCabin.hasRoom()}")
+        getRoom() //same as squareCabin.getRoom()
+        println("Has room? ${hasRoom()}") // same as println("Has room? ${squareCabin.hasRoom()}")
 		getRoom()
     }
         
@@ -38,6 +39,7 @@ fun main() {
     }
 }
 
+// the class and most of it's variables need to be defined as abstract so it can have undefined variables and used as a parent class 
 abstract class Dwelling(private var residents: Int){
 	
     abstract val buildingMaterial: String
@@ -61,7 +63,7 @@ abstract class Dwelling(private var residents: Int){
 
 class SquareCabin(residents: Int,
     val length: Double) : Dwelling(residents){
-    
+    // the variables need to be defined as override so they can change the values defined in the parent class
     override val buildingMaterial = "Wood"
     override val capacity = 6
     
@@ -71,6 +73,7 @@ class SquareCabin(residents: Int,
     
 }
 
+// set as open class so it can be used as a parent class to the RoundTower class defined after this one
 open class RoundHut(residents: Int, 
     val radius: Double) : Dwelling(residents) {
     
@@ -94,6 +97,8 @@ class RoundTower(residents: Int, radius: Double,
     override val capacity = 4 * floors
     
     override fun floorArea(): Double{
-        return super.floorArea() * floors
+        return super.floorArea() * floors // could be used the same function used in the RoundHut class, however it's easier to call the function used in that class and change it to a superclass using super.function()
     }
 }
+
+//https://developer.android.com/codelabs/basic-android-kotlin-training-classes-and-inheritance?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-kotlin-unit-2-pathway-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-training-classes-and-inheritance#2
