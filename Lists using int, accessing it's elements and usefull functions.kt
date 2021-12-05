34444cd6eefc5fea70bf0ca6ecdb9a6737c4576a
PedroTorrado <88887442+PedroTorrado@users.noreply.github.com>
2021-12-05 15:31:38 +0000

    Update Lists using int, accessing it's elements and usefull functions.kt

--- Lists using int, accessing it's elements and usefull functions.kt
+++ Lists using int, accessing it's elements and usefull functions.kt
@@ -23,3 +23,5 @@ fun main() {
     println("Contains 4? ${Numbers.contains(4)}")
     println("Contains 7? ${Numbers.contains(7)}")
 }
+
+//https://developer.android.com/codelabs/basic-android-kotlin-training-lists?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-kotlin-unit-2-pathway-3%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-training-lists#1
