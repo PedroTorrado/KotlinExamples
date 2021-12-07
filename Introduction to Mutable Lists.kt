6199954fbca6ff93d406f2d1005844fe27475439
PedroTorrado <88887442+PedroTorrado@users.noreply.github.com>
2021-12-07 22:47:43 +0000

    Update Introduction to Mutable Lists.kt

--- Introduction to Mutable Lists.kt
+++ Introduction to Mutable Lists.kt
@@ -1,14 +1,18 @@
 fun main() {
+	//defining a MutableList and giving it the entrees value
 	val entrees: MutableList<String> = mutableListOf() 
+	
     println("Entrees: $entrees")
     
+    //add String to the list
     println("Add noodles: ${entrees.add("noodles")}")
     println("Entrees: $entrees")
     println("Add spaghetti: ${entrees.add("spaghetti")}")
     println("Entrees: $entrees")
-
+	//defining a list to sum the string variables to the MutableList
 	val moreItems = listOf("ravioli", "lasagna", "fettuccine")
     
+    //Adding all the variables from a list to a MutableList
     println("Add list: ${entrees.addAll(moreItems)}")
     println("Entrees: $entrees")
 	
@@ -20,14 +24,18 @@ fun main() {
     println("Remove spaghetti: ${entrees.remove("spaghetti")}")
     println("Entrees: $entrees")
     
+	//Trying to remove an unexistent item from a list results on a Boolean result of False
 	println("Removing an item that doesn't exist: ${entrees.remove("rice")}")
     println("Entrees: $entrees")
 	
+	//It's possible to remove the items from a list using it's element instead of only it's content
     println("Remove first element: ${entrees.removeAt(0)}")
     println("Entrees: $entrees")
-
+	
+	//It's possible to verify if a list is empty or not using the .isEmpty() function
     println("Empty? ${entrees.isEmpty()}")
     
+	//It's possible to completely clear the items from a list using only .clear() function
    	entrees.clear()
     println("Entrees: $entrees")
     
