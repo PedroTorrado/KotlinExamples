e557829c78ccc31cef6758b718a0aa9201283a33
PedroTorrado <88887442+PedroTorrado@users.noreply.github.com>
2021-12-07 22:30:54 +0000

    Update For and While loops.kt

--- For and While loops.kt
+++ For and While loops.kt
@@ -1,7 +1,12 @@
 fun main(){
+	
+	//list and variables defined to use with loops
     val guestsPerFamily = listOf(2, 4, 1, 3)
     var totalGuests = 0
     var index = 0
+	
+	//while loop used to count the guests 
+	//while function is true, repeat
     while (index < guestsPerFamily.size){
         totalGuests += guestsPerFamily[index]
         index++
@@ -9,8 +14,9 @@ fun main(){
     println("TotalGuest Count: $totalGuests")
     
 	println()
-
+	
     val names = listOf("Jessica", "Henry", "Alicia", "Jose")
+	//for loop to print all the names and using .lenght it also displays the number of characters each name has
     for (name in names){
         println("$name - Number of characters: ${name.length}")
     }
