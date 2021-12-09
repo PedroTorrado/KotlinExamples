01708f60804f514ca44e50af0b6522670bddeec3
PedroTorrado <88887442+PedroTorrado@users.noreply.github.com>
2021-12-09 22:54:11 +0000

    Update RandomCardsList.kt

--- RandomCardsList.kt
+++ RandomCardsList.kt
@@ -8,7 +8,7 @@ var diamonds = listOf("Ace of diamonds", "2 of diamonds", "3 of diamonds", "4 of
 var hearts = listOf("A of hearts", "2 of hearts", "3 of hearts", "4 of hearts", "5 of hearts", "6 of hearts", "7 of hearts", "8 of hearts", "9 of hearts", "10 of hearts", "Jack of hearts", "Queen of hearts", "King of hearts")
 var jacks = listOf("Joker", "Joker")
 
-var empty = false
+var i = 1;
 
 fun main() {
     
@@ -31,10 +31,11 @@ fun main() {
     println("Full Deck: $deck")
 
     println()
-          
-     while(empty == false){ 
-        empty = deck.isEmpty()
+    var empty = deck.isEmpty()
+    
+    while(empty == false){ 
         take_a_card() 
+        empty = deck.isEmpty()
     }
     
     if(empty == true){
@@ -47,11 +48,12 @@ fun main() {
 
 public fun take_a_card(){
 
+    println("Turn Number: $i")
+    i++
+    
     val card = deck.random()
-    if (empty == false) {
     println(card)
     deck.remove(card)        
     println(deck)
     println()
-    }
 }
