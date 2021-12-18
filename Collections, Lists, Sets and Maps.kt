e307006d81d677314720363f6b526b02cefece5b
PedroTorrado <88887442+PedroTorrado@users.noreply.github.com>
2021-12-18 19:12:01 +0000

    Update and rename Collections, Lists, Sets.kt to Collections, Lists, Sets and Maps.kt

---
+++ Collections, Lists, Sets and Maps.kt
@@ -0,0 +1,38 @@
+fun main() {
+    val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
+    println("list:   ${numbers}")
+    println("sorted: ${numbers.sorted()}")
+    val setOfNumbers = numbers.toSet()
+    println("set:    ${setOfNumbers}")
+    val set1 = setOf(1,2,3)
+    val set2 = mutableSetOf(3,2,1)
+    println("$set1 == $set2: ${set1 == set2}")
+    println("contains 7: ${setOfNumbers.contains(7)}")
+    println()
+    // Maps 
+    println("-- Maps --")
+    val peopleAges = mutableMapOf<String, Int>(
+        "Fred" to 30,
+        "Ann" to 23
+    )
+    println(peopleAges)
+    peopleAges.put("Barbara", 42)
+    peopleAges["Joe"] = 51
+    println(peopleAges)
+    peopleAges["Fred"] = 31
+    println(peopleAges)
+}
+
+/*
+Output:
+list:   [0, 3, 8, 4, 0, 5, 5, 8, 9, 2]
+sorted: [0, 0, 2, 3, 4, 5, 5, 8, 8, 9]
+set:    [0, 3, 8, 4, 5, 9, 2]
+[1, 2, 3] == [3, 2, 1]: true
+contains 7: false
+
+-- Maps --
+{Fred=30, Ann=23}
+{Fred=30, Ann=23, Barbara=42, Joe=51}
+{Fred=31, Ann=23, Barbara=42, Joe=51}
+*/
